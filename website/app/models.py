fca1832c85fead7c54857d546a3ff5103e0a6fb8
taylorreece <taylor.reece@gmail.com>
2017-11-15 22:53:32 -0600

    Timestamps have timezone; pulled id column type to a variable

--- website/app/models.py
+++ website/app/models.py
@@ -1,16 +1,19 @@
 from app import db
 
+if db.engine.name == 'postgresql':
+    from app.sqlalchemy_extensions.uuid_column import UUID
+    id_column_type = UUID
+    id_column_server_default = db.text("uuid_in(md5(random()::text || now()::text)::cstring)")
+else:
+    id_column_type = db.Integer
+    id_column_server_default = None
 # Define a base model for other database tables to inherit
 # We'll use UUIDs in postgresql, but just ids for testing
 class Base(db.Model):
     __abstract__  = True
-    if db.engine.name == 'postgresql':
-        from app.sqlalchemy_extensions.uuid_column import UUID
-        id = db.Column(UUID, primary_key=True, server_default=db.text("uuid_in(md5(random()::text || now()::text)::cstring)"))
-    else:
-        id = db.Column(db.Integer, primary_key=True)
-    created  = db.Column(db.DateTime, server_default=db.func.current_timestamp())
-    modified = db.Column(db.DateTime, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    id = db.Column(id_column_type, primary_key=True, server_default=id_column_server_default)
+    created  = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def save(self):
         db.session.add(self)
