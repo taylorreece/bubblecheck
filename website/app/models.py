4f55b8026ad7a74adcf7bfb148acd6cec0fa50ee
taylorreece <taylor.reece@gmail.com>
2017-11-15 22:27:10 -0600

    Added UUID columns for postgresql; updated models to use UUID ids in postgresql, ints in development

--- website/app/models.py
+++ website/app/models.py
@@ -1,11 +1,16 @@
 from app import db
 
 # Define a base model for other database tables to inherit
+# We'll use UUIDs in postgresql, but just ids for testing
 class Base(db.Model):
     __abstract__  = True
-    id       = db.Column(db.Integer, primary_key=True)
-    created  = db.Column(db.DateTime, default=db.func.current_timestamp())
-    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    if db.engine.name == 'postgresql':
+        from app.sqlalchemy_extensions.uuid_column import UUID
+        id = db.Column(UUID, primary_key=True, server_default=db.text("uuid_in(md5(random()::text || now()::text)::cstring)"))
+    else:
+        id = db.Column(db.Integer, primary_key=True)
+    created  = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def save(self):
         db.session.add(self)
