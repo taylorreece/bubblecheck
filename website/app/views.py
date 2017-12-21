84ba02d55435596ba40b59ef45d92c76d5622d38
taylorreece <taylor.reece@zuerchertech.com>
2017-12-21 15:58:57 -0600

    Cleaned up default templates

--- website/app/views.py
+++ website/app/views.py
@@ -3,4 +3,8 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
