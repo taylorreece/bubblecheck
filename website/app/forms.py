643c1157814c87149c7ec78df39b0bbb4e477a17
Taylor Reece <taylor.reece@zuerchertech.com>
2018-05-14 14:49:59 -0500

    Added a course form

--- website/app/forms.py
+++ website/app/forms.py
@@ -5,10 +5,20 @@ from wtforms import PasswordField
 from wtforms import SelectField
 from wtforms import StringField
 from wtforms import validators
+from flask_wtf import RecaptchaField
 
 # ===================================================
 class LoginForm(Form):
     ''' Form for login screen '''
-    email       = StringField("Email Address", [validators.Email(message=u"That's not a valid email address.")])
+    email       = StringField("Email Address", [validators.Email(message="That's not a valid email address.")])
     password    = PasswordField("Password")
-    remember_me = BooleanField(u'Remember Me')
+    remember_me = BooleanField("Remember Me")
+
+# ===================================================
+class CourseForm(Form):
+    name        = StringField("Course Name", [validators.DataRequired(message="Course name required")])
+    sections    = FieldList(
+        StringField("Section"),
+        min_entries = 1
+    )
+    
