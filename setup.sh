06bfcc979189306209e39836f54fd598f3cf3d7b
Taylor Reece <taylor.reece@zuerchertech.com>
2018-07-06 16:29:02 -0500

    Added vue directory

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ python-venv/bin/pip install -r requirements.txt
 # Create and use node virtual environment
 python-venv/bin/python -m nodeenv node-venv
 source node-venv/bin/activate
-npm install -g vue-cli
+npm install --global @vue/cli @vue/cli-init
