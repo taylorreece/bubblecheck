5e635a78a6f80c6b36c077a34f231a953cfefba6
taylorreece <taylor.reece@gmail.com>
2018-07-07 13:58:59 -0500

    Maybe this will work?

--- build/build_vue.sh
+++ build/build_vue.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex 
 
 CWD=$pwd
 DIR=$(dirname $0)
@@ -6,6 +7,7 @@ cd $DIR/..
 source node-venv/bin/activate
 npm install bubblecheck-vue
 cd bubblecheck-vue
+npm install --global @vue/cli @vue/cli-init
 npm run build
 
 cd $CWD
