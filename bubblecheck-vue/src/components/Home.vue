bdcd3602bbd6cd7d3d90dedcb09ca1cad26a3931
taylorreece <taylor.reece@gmail.com>
2018-07-14 00:07:42 -0500

    More responsive welcome screen

--- bubblecheck-vue/src/components/Home.vue
+++ bubblecheck-vue/src/components/Home.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--6-col mdl-cell--3-offset">
-      <h2 class="mdl-card__title-text">Welcome, Teachers!</h2>
-        After months of development and testing, I'm excited to present this production version of BubbleCheck. This software is designed to give you, the educator, thorough, immediate assessment feedback so that you can create dynamic, data-driven curriculum. A special "thank you" to everyone who beta-tested the software.  A registration link is above! -- Taylor
+  <div>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--6-col-desktop mdl-cell--3-offset-desktop mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--4-col-phone">
+        <h2 class="mdl-card__title-text">Welcome, Teachers!</h2>
+          After months of development and testing, I'm excited to present this production version of BubbleCheck. This software is designed to give you, the educator, thorough, immediate assessment feedback so that you can create dynamic, data-driven curriculum. A special "thank you" to everyone who beta-tested the software.  A registration link is above! -- Taylor
+      </div>
     </div>
   </div>
 </template>
