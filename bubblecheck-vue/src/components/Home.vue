ee0e35e426ee651040f35f7124ff0fd7fdff3237
taylorreece <taylor.reece@gmail.com>
2018-07-15 22:44:46 -0500

    cleaned up some css

--- bubblecheck-vue/src/components/Home.vue
+++ bubblecheck-vue/src/components/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--6-col-desktop mdl-cell--3-offset-desktop mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--4-col-phone">
-        <h2 class="mdl-card__title-text">Welcome, Teachers!</h2>
+      <div class="mdl-cell mdl-card mdl-shadow--4dp mdl-cell--6-col-desktop mdl-cell--3-offset-desktop mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--4-col-phone">
+        <div class="mdl-card__title">
+          <h2 class="mdl-card__title-text">Welcome, Teachers!</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
           After months of development and testing, I'm excited to present this production version of BubbleCheck. This software is designed to give you, the educator, thorough, immediate assessment feedback so that you can create dynamic, data-driven curriculum. A special "thank you" to everyone who beta-tested the software.  A registration link is above! -- Taylor
+        </div>
       </div>
     </div>
   </div>
--- bubblecheck-vue/src/components/Login.vue
+++ bubblecheck-vue/src/components/Login.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class="mdl-card login-card mdl-shadow--4dp">
-        <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
-            <h2 class="mdl-card__title-text">Log in</h2>
-        </div>
-        <div class="mdl-card__supporting-text">
-            <form class="form-signin" method="POST" action="/user/login">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input" type="email" id="email" name="email">
-                    <label class="mdl-textfield__label" for="email">Email Address</label>
-                </div>
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input" type="password" id="password" name="password">
-                    <label class="mdl-textfield__label" for="password">Password</label>
-                </div>
-                <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored ">Sign In</button>
-                | <a href="#">Forgot my password</a>
-            </form>
+    <div class="login-page">
+        <div class="mdl-card login-card mdl-shadow--4dp">
+            <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
+                <h2 class="mdl-card__title-text">Log in</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+                <form class="form-signin" method="POST" action="/user/login">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input class="mdl-textfield__input" type="email" id="email" name="email">
+                        <label class="mdl-textfield__label" for="email">Email Address</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input class="mdl-textfield__input" type="password" id="password" name="password">
+                        <label class="mdl-textfield__label" for="password">Password</label>
+                    </div>
+                    <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored ">Sign In</button>
+                    | <a href="#">Forgot my password</a>
+                </form>
+            </div>
         </div>
     </div>
 </template>
 
 <style>
-.mdl-layout__content {
+.login-page {
     align-items: center;
     display: flex;
     justify-content: center;
