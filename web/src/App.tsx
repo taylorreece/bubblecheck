4f4d9a2c154c4c54b822dcad84e55406b5ca9480
Taylor Reece <taylor.reece@gmail.com>
2019-11-18 14:46:28 -0600

    Added some basic material-ui components.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import {Button} from "@material-ui/core";
+import {AppBar, Button, CssBaseline, IconButton, makeStyles, Toolbar, Typography} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
 import React from "react";
 import {AuthConsumer, AuthProvider} from "./contexts/AuthContext";
 
@@ -12,18 +13,52 @@ function PrintUserInfo(props: any) {
   );
 }
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <Button variant="contained" color="primary" href="/api/users/oauth/cognito/login">Login</Button>
-      <Button variant="contained" color="secondary" href="/api/users/logout">Logout</Button>
-      <AuthProvider>
-        <AuthConsumer>
-          {(authctx) => <PrintUserInfo user={authctx.user} />}
+function LoginLogoutButton(props: any) {
+  if (props.user.isAuthenticated) {
+    return (<Button color="inherit" href="/api/users/logout">Logout</Button>);
+  } else {
+    return (<Button color="inherit" href="/api/users/oauth/cognito/login">Login</Button>);
+  }
+}
 
-        </AuthConsumer>
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const App: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <AuthProvider>
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <Menu />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                bubblecheck.app
+              </Typography>
+              <AuthConsumer>
+                {(authctx) => <LoginLogoutButton user={authctx.user} />}
+              </AuthConsumer>
+            </Toolbar>
+          </AppBar>
+            <AuthConsumer>
+              {(authctx) => <PrintUserInfo user={authctx.user} />}
+            </AuthConsumer>
+        </div>
       </AuthProvider>
-    </div>
+    </React.Fragment>
   );
 };
 
--- web/src/api/Api.tsx
+++ web/src/api/Api.tsx
@@ -19,7 +19,6 @@ class ApiInterface {
                 teacher_name: "",
             };
         }
-
     }
 }
 
