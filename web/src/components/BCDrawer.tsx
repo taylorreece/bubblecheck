6a45a613e94323e3ba063a6c10cecda9bebb5e61
Taylor Reece <taylor.reece@gmail.com>
2020-01-03 16:20:41 -0600

    Quick addition for a demo.

--- web/src/components/BCDrawer.tsx
+++ web/src/components/BCDrawer.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {AuthConsumer} from "../contexts/AuthContext";
 
 const BCDrawer: React.FC = () => {
     return (
         <div>
-            Hello World!
+            <AuthConsumer>
+                {(authctx) => <div>
+			<strong>Email:</strong> { authctx.user.email }<br />
+			<strong>Name:</strong> { authctx.user.teacher_name }<br />
+			<strong>UserID:</strong> { authctx.user.usersid }<br />
+		</div>}
+            </AuthConsumer>
         </div>
     );
 };
