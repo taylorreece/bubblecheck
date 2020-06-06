2bef42a62ab6adc32c2ec09adb384e0f110c0c1d
Taylor Reece <taylor.reece@gmail.com>
2020-06-05 23:34:38 -0500

    Fix up some linting.

--- web/src/components/BCDrawer.tsx
+++ web/src/components/BCDrawer.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import {AuthConsumer} from "../contexts/AuthContext";
+import { AuthConsumer } from "../contexts/AuthContext";
+
+const myPage = (authctx: any) => {
+  return (
+    <div>
+      <strong>Email:</strong> {authctx.user.email}
+      <br />
+      <strong>Name:</strong> {authctx.user.teacher_name}
+      <br />
+      <strong>UserID:</strong> {authctx.user.usersid}
+      <br />
+    </div>
+  );
+};
 
 const BCDrawer: React.FC = () => {
-    return (
-        <div>
-            <AuthConsumer>
-                {(authctx) => <div>
-			<strong>Email:</strong> { authctx.user.email }<br />
-			<strong>Name:</strong> { authctx.user.teacher_name }<br />
-			<strong>UserID:</strong> { authctx.user.usersid }<br />
-		</div>}
-            </AuthConsumer>
-        </div>
-    );
+  return (
+    <div>
+      <AuthConsumer>{(authctx) => myPage(authctx)}</AuthConsumer>
+    </div>
+  );
 };
 
 export default BCDrawer;
